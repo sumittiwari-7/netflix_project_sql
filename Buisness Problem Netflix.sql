7d23f661fd0f89f7a7f0e8b78d0c96c2dfbd3905
Sumit Tiwari <sumitstiwari07@gmail.com>
2026-02-10 11:54:22 +0530

    Create Buisness Problem Netflix.sql

---
+++ Buisness Problem Netflix.sql
@@ -0,0 +1,13 @@
+--  Business Problems 
+1. find content added in the last 5 years.
+2. Find the Directors with highest Netflix Presence.
+3. Find Genre wise content wise distribution.
+4. Find TV SHOWS with more than 5 seasons.
+5. Count the number of content items in each Genre.
+6. Find each year and the average numbers of content release by India on Netflix
+7. List all the movies that are listed as Documentries.
+8. Find how many movie actor 'salman khan' appeared in the last 10 years.
+9. Find the top 10 Actors who have appeared in the highest number of movies produced in India.
+10. Categorize the content based on the presence of keywords 'kill' and 'violence' in the description.
+11. How has Netflix’s content volume grown over time, and which year had the highest additions.
+12. Which countries dominate Netflix’s catalog and what is their percentage contribution.
